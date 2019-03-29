94fe963ea6829e464f59f08ac7f46a885cdc54e3
svenhsia <svenhsia@gmail.com>
2019-03-30 00:47:20 +0100

    add context tree module

--- context-tree-model.py
+++ context-tree-model.py
@@ -1,6 +1,11 @@
 from collections import defaultdict
 import numpy as np
+import pandas as pd
 from scipy.special import gamma as gamma_function
+from cvxopt import matrix, solvers
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def all_contexts(alphabet, D=3):
     assert D > 1
@@ -37,6 +42,7 @@ def compute_avg_weight(rho_x, rho_y, theta_x, theta_y):
     """
     Nx = np.sum(rho_x)
     Ny = np.sum(rho_y)
+    assert Nx > 0  and Ny > 0
     avg_weight = rho_x.reshape((-1, 1)) * theta_x / Nx + rho_y.reshape((-1, 1)) * theta_y / Ny
     return avg_weight
 
@@ -70,10 +76,10 @@ def compute_K(gamma, betas, sigma, avg_weight):
     rslt[non_occurred] = 1
     return rslt
 
-def compute_kernel_value(X_id, Y_id, counts_rho, counts_theta, epsilon, gamma, betas, sigma):
-    d = counts_theta.shape[-1]
+def compute_kernel_value(rho_X, rho_Y, theta_X, theta_Y, epsilon, gamma, betas, sigma):
+    d = theta_X.shape[-1]
     # size (num_contexts, d)
-    avg_weight = compute_avg_weight(counts_rho[X_id], counts_rho[Y_id], counts_theta[X_id], counts_theta[Y_id])
+    avg_weight = compute_avg_weight(rho_X, rho_Y, theta_X, theta_Y)
     # size (num_contexts, )
     K = compute_K(gamma, betas, sigma, avg_weight)
     Gamma = K
@@ -98,53 +104,160 @@ def compute_kernel_value(X_id, Y_id, counts_rho, counts_theta, epsilon, gamma, b
         Gamma = Gamma_new
     return Gamma[0]
 
-X = [
-    "GGAGAATCATTTGAACCCGGGAGGTGGAGGTTGCCGTGAGCTGAGATTGCGCCATTGCACTCCAGCCTGGGCAACAAGAGCAAAACTCTGTCTCACAAAAC",
-    "TGCAAATCTGTAAGCATTTCTCAGGCAATGAATTATGTCAACACAATTGCACCATCATTGATGGACTTGGAAATGCAGACAGAACTGAAGAGGAGCGTCTC"
-]
-y = [0, 1]
-
-ALPHABET = ['A', 'T', 'G', 'C']
-
-n_samples = len(X)
+ALPHABET = ['A', 'C', 'G', 'T']
 D = 3
-epsilon = 0.4
+CONTEXTS = all_contexts(ALPHABET, D=D)
+logging.info("All contexts: {}".format(CONTEXTS))
+
+epsilon = 1. / len(ALPHABET)
 num_dirichelet = 1
 gamma = [1 / num_dirichelet for _ in range(num_dirichelet)]
 betas = np.full((num_dirichelet, len(ALPHABET)), 0.5)
-sigma = 1
+sigma = 2
+lamdas = [0.01, 0.1, 1, 10]
 
-K = np.zeros((n_samples, n_samples), dtype=float)
+benckmark = pd.DataFrame(columns=['set 0', 'set 1', 'set 2'], index=lamdas)
+benckmark.index.name = 'lambda'
+for lamda_idx, lamda in enumerate(lamdas):
+    logging.info("lamda = {}".format(lamda))
+    for dataset_id in range(3):
+        logging.info("Data set No.{}".format(dataset_id))
+        train_X = pd.read_csv('./data/Xtr{}.csv'.format(dataset_id), header=0, index_col=0).values.flatten()
+        test_X = pd.read_csv('./data/Xte{}.csv'.format(dataset_id), header=0, index_col=0).values.flatten()
+        train_Y = pd.read_csv('./data/Ytr{}.csv'.format(dataset_id), header=0, index_col=0)['Bound']
+        # train_Y[train_Y==0] = -1
+        logging.info("Loaded {} train samples, {} test samples".format(len(train_X), len(test_X)))
 
-CONTEXTS = all_contexts(ALPHABET, D=D)
+        n_samples = len(train_X)
+        filter_idx = np.full((n_samples,), True)
+        choose_idx = np.random.choice(range(n_samples), size=int(0.2*n_samples), replace=False)
+        filter_idx[choose_idx] = False
+        val_X = train_X[~filter_idx]
+        val_Y = train_Y[~filter_idx]
+        train_X = train_X[filter_idx]
+        train_Y = train_Y[filter_idx]
+        logging.info("Split train samples into {} validation samples and {} training samples".format(len(val_X), len(train_X)))
+        
+        n_train = len(train_X)
 
-counts_rho, counts_theta = count_occurrences(X, CONTEXTS, ALPHABET, D=D)
-print(counts_rho)
-print(counts_theta)
+        logging.info("Computing kernel matrix on training set")
+        K = np.zeros((n_train, n_train), dtype=float)
+        counts_rho, counts_theta = count_occurrences(train_X, CONTEXTS, ALPHABET, D=D)
 
-for i in range(n_samples):
-    K[i, i] = compute_kernel_value(i, i, counts_rho, counts_theta, epsilon, gamma, betas, sigma)
-    for j in range(i+1, n_samples):
-        K[i, j] = compute_kernel_value(i, j, counts_rho, counts_theta, epsilon, gamma, betas, sigma)
-        K[j, i] = K[i, j]
+        for i in range(n_train):
+            K[i, i] = compute_kernel_value(counts_rho[i], counts_rho[i], counts_theta[i], counts_theta[i], epsilon, gamma, betas, sigma)
+            for j in range(i+1, n_train):
+                K[i, j] = compute_kernel_value(counts_rho[i], counts_rho[j], counts_theta[i], counts_theta[j], epsilon, gamma, betas, sigma)
+                K[j, i] = K[i, j]
+        
+        P = matrix(2 * K, tc='d')
+        q = matrix(- 2 * train_Y, tc='d')
+        # G = matrix(np.diag(np.concatenate((np.array(train_Y), -np.array(train_Y)))), tc='d')
+        G = matrix(np.concatenate((np.diag(train_Y), np.diag(-train_Y)), axis=0), tc='d')
+        h = matrix(np.concatenate((np.ones(n_train) / (2 * lamda * n_train), np.zeros(n_train))), tc='d')
 
-print(K)
+        sol = solvers.qp(P, q, G, h)
+        # print(sol['x'])
+        alpha = sol['x']
 
-y = np.array(y)
+        supp_alpha = []
+        supp_idx = []
+        for i, ele in enumerate(alpha):
+            if ele != 0:
+                supp_idx.append(i)
+                supp_alpha.append(ele)
+        
+        logging.info("Computing on validation set")
+        n_support = len(supp_alpha)
+        n_val = len(val_X)
+        K_val = np.zeros((n_support, n_val), dtype=float)
 
-# P = matrix(2 * K, tc='d')
-# q = matrix(- 2 * y, tc='d')
-# G = matrix(np.concatenate((np.diag(y), np.diag(-y)), axis=0), tc='d')
-# h = matrix(np.concatenate((np.ones(n) / (2 * lamda * n), np.zeros(n))), tc='d')
+        counts_rho_val, counts_theta_val = count_occurrences(val_X, CONTEXTS, ALPHABET, D=D)
+        for i in range(n_support):
+            for j in range(n_val):
+                supp_i = supp_idx[i]
+                K_val[i, j] = compute_kernel_value(counts_rho[supp_i], counts_rho_val[j], counts_theta[supp_i], counts_theta_val[j], epsilon, gamma, betas, sigma)
 
-# sol = solvers.qp(P, q, G, h)
-# # print(sol['x'])
-# return sol['x']
+        pred_val = np.dot(K_val.T, np.array(supp_alpha).reshape((-1, 1))) >= 0
+        pred_val = pred_val.astype(int).flatten()
 
+        val_accuracy = np.mean(pred_val == val_Y)
+        logging.info("Obtain validation accuracy: {}".format(val_accuracy))
+        benckmark.iloc[lamda_idx, dataset_id] = val_accuracy
 
+        logging.info("Computing on test set")
+        n_test= len(test_X)
+        K_test = np.zeros((n_support, n_test), dtype=float)
+        counts_rho_test, counts_theta_test = count_occurrences(test_X, CONTEXTS, ALPHABET, D=D)
+        for i in range(n_support):
+            for j in range(n_test):
+                supp_i = supp_idx[i]
+                K_test[i, j] = compute_kernel_value(counts_rho[supp_i], counts_rho_test[j], counts_theta[supp_i], counts_theta_test[j], epsilon, gamma, betas, sigma)
 
+        pred_test = np.dot(K_test.T, np.array(supp_alpha).reshape((-1, 1))) >= 0
+        pred_test = pred_test.astype(int)
 
+        pred_test = pd.DataFrame(pred_test, columns=['Bound'])
+        pred_test.index.name = 'Id'
+        pred_test.to_csv("pred_te{}_lamda_{}.csv".format(dataset_id, lamda))
 
+benckmark.to_csv('benckmark.csv')
 
+best_lamda_index = benckmark.mean(axis=1).idxmax()
+best_lamda = lamdas[int(best_lamda_index)]
 
+logging.info("Choosing best lamda: {}".format(best_lamda))
+lamda = best_lamda
+for i in range(3):
+    logging.info("Data set No.{}".format(i))
+    train_X = pd.read_csv('./data/Xtr{}.csv'.format(i), header=0, index_col=0).values.flatten()
+    test_X = pd.read_csv('./data/Xte{}.csv'.format(i), header=0, index_col=0).values.flatten()
+    train_Y = pd.read_csv('./data/Ytr{}.csv'.format(i), header=0, index_col=0)['Bound']
+    # train_Y[train_Y==0] = -1
+    logging.info("Loaded {} train samples, {} test samples".format(len(train_X), len(test_X)))
+    
+    n_train = len(train_X)
 
+    logging.info("Computing kernel matrix on training set")
+    K = np.zeros((n_train, n_train), dtype=float)
+    counts_rho, counts_theta = count_occurrences(train_X, CONTEXTS, ALPHABET, D=D)
+
+    for i in range(n_train):
+        K[i, i] = compute_kernel_value(counts_rho[i], counts_rho[i], counts_theta[i], counts_theta[i], epsilon, gamma, betas, sigma)
+        for j in range(i+1, n_train):
+            K[i, j] = compute_kernel_value(counts_rho[i], counts_rho[j], counts_theta[i], counts_theta[j], epsilon, gamma, betas, sigma)
+            K[j, i] = K[i, j]
+    
+    P = matrix(2 * K, tc='d')
+    q = matrix(- 2 * train_Y, tc='d')
+    # G = matrix(np.diag(np.concatenate((np.array(train_Y), -np.array(train_Y)))), tc='d')
+    G = matrix(np.concatenate((np.diag(train_Y), np.diag(-train_Y)), axis=0), tc='d')
+    h = matrix(np.concatenate((np.ones(n_train) / (2 * lamda * n_train), np.zeros(n_train))), tc='d')
+
+    sol = solvers.qp(P, q, G, h)
+    # print(sol['x'])
+    alpha = sol['x']
+
+    supp_alpha = []
+    supp_idx = []
+    for i, ele in enumerate(alpha):
+        if ele != 0:
+            supp_idx.append(i)
+            supp_alpha.append(ele)
+    n_support = len(supp_alpha)
+
+    logging.info("Computing on test set")
+    n_test= len(test_X)
+    K_test = np.zeros((n_support, n_test), dtype=float)
+    counts_rho_test, counts_theta_test = count_occurrences(test_X, CONTEXTS, ALPHABET, D=D)
+    for i in range(n_support):
+        for j in range(n_test):
+            supp_i = supp_idx[i]
+            K_test[i, j] = compute_kernel_value(counts_rho[supp_i], counts_rho_test[j], counts_theta[supp_i], counts_theta_test[j], epsilon, gamma, betas, sigma)
+
+    pred_test = np.dot(K_test.T, np.array(supp_alpha).reshape((-1, 1))) >= 0
+    pred_test = pred_test.astype(int)
+
+    pred_test = pd.DataFrame(pred_test, columns=['Bound'])
+    pred_test.index.name = 'Id'
+    pred_test.to_csv("pred_total_te{}_lamda_{}.csv".format(i, lamda))
